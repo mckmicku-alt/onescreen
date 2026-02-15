b8b3e69a9418f30c2734e09ff278791c64eadf15
Michal <mckmicku@gmail.com>
2026-02-15 16:21:49 +0100

    Update landing: coming soon lamps + fixes

--- src/components/ComingSoonBanner.tsx
+++ src/components/ComingSoonBanner.tsx
@@ -1,9 +1,15 @@
 /**
+<<<<<<< HEAD
  * Komponent ComingSoonBanner – wyświetla gradientowe tło z nagłówkiem.
  * Dodano tekst „Coming Soon” na środku ekranu oraz dwie lampy oświetlające napis.
  */
 import Image from "next/image";
 
+=======
+ * Komponent ComingSoonBanner – wersja zgodna z Vite/React.
+ * Używa standardowych tagów <img> zamiast next/image.
+ */
+>>>>>>> c10092a (Naprawa: użycie tagów img w ComingSoonBanner)
 const ComingSoonBanner = () => {
   return (
     <section className="relative w-full py-32 overflow-hidden">
@@ -17,12 +23,17 @@ const ComingSoonBanner = () => {
         }}
       />
       {/* Lampy po lewej i prawej stronie */}
+<<<<<<< HEAD
       <Image
+=======
+      <img
+>>>>>>> c10092a (Naprawa: użycie tagów img w ComingSoonBanner)
         src="/lights/left-lamp.png"
         alt="Lewy reflektor"
         width={450}
         height={450}
         className="absolute -top-4 left-0 w-[30vw] max-w-sm select-none pointer-events-none"
+<<<<<<< HEAD
         priority
       />
       <Image
@@ -32,6 +43,15 @@ const ComingSoonBanner = () => {
         height= {450}
         className="absolute -top-4 right-0 w-[30vw] max-w-sm select-none pointer-events-none"
         priority
+=======
+      />
+      <img
+        src="/lights/right-lamp.png"
+        alt="Prawy reflektor"
+        width={450}
+        height={450}
+        className="absolute -top-4 right-0 w-[30vw] max-w-sm select-none pointer-events-none"
+>>>>>>> c10092a (Naprawa: użycie tagów img w ComingSoonBanner)
       />
       {/* Kontener na treść – napis i opis */}
       <div className="relative z-10 flex flex-col items-center justify-center px-4 text-center">
