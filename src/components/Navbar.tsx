7f0f622514b621358ffbbe7907cd771fc1c43c3a
Michal <mckmicku@gmail.com>
2026-02-15 17:10:47 +0100

    UI: remove unused navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,12 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 right-0 z-50">
       <div className="mx-auto max-w-6xl px-6">
         <div className="mt-4 flex items-center justify-between">
-          <a
-            href="/"
-            className="text-xl font-extrabold tracking-tight text-white"
-          >
+          <a href="/" className="text-xl font-extrabold tracking-tight text-white">
             One<span className="text-primary">Screen</span>
           </a>
 
-          {/* Pusta prawa strona – bez linków typu Przeglądaj/Platformy */}
-          <div className="w-16" />
+          {/* brak linków */}
+          <div className="w-10" />
         </div>
       </div>
     </header>
